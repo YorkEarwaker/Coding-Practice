5c34522363358f78565cdcc3a8d79d864b4fea35
York Earwaker <yorkearwaker@yahoo.com>
2026-04-16 14:54:22 +0100

    update

---
+++ ubuntu/cpa/hwd2/src/hwd-kr.c
@@ -0,0 +1,8 @@
+// after Kerrnighan & Ritchie, The C Programming Language
+
+#include <stdio.h>
+
+main () 
+{
+    printf("hello, world\n");
+}
---
+++ ubuntu/cpa/hwd2/src/hwd.c
@@ -0,0 +1,8 @@
+// after Kerrnigham & Ritchie, The C Programming Language
+#include <stdio.h>
+
+// int added to original ANSI standard code from book, as per requirement since C99, 
+int main ()
+{
+    printf("hello, world\n");
+}
