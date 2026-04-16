1dc776e9fbac822e31f3d77539866c520b386c24
York Earwaker <yorkearwaker@yahoo.com>
2026-04-16 15:29:26 +0100

    update

--- ubuntu/cpa/hwd2/src/hwd-kr.c
+++ ubuntu/cpa/hwd2/src/hwd-kr.c
@@ -1,7 +1,8 @@
-// after Kerrnighan & Ritchie, The C Programming Language
-
+// after Kernighan & Ritchie, The C Programming Language, 2nd Ed
+// ANSI C code as per book
 #include <stdio.h>
 
+// compile with errors due to no int before main, int a requirement since C99
 main () 
 {
     printf("hello, world\n");
--- ubuntu/cpa/hwd2/src/hwd.c
+++ ubuntu/cpa/hwd2/src/hwd.c
@@ -1,7 +1,7 @@
-// after Kerrnigham & Ritchie, The C Programming Language
+// after Kernigham & Ritchie, The C Programming Language, 2nd Ed
 #include <stdio.h>
 
-// int added to original ANSI standard code from book, as per requirement since C99, 
+// int added to original ANSI standard code from book, int a requirement since C99, 
 int main ()
 {
     printf("hello, world\n");
