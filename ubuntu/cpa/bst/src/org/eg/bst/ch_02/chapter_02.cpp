b29182046554fe8ddfd8251caef25e538c254bce
York Earwaker <yorkearwaker@yahoo.com>
2026-04-24 17:13:19 +0100

    update

--- ubuntu/cpa/bst/src/org/eg/bst/ch_02/chapter_02.cpp
+++ ubuntu/cpa/bst/src/org/eg/bst/ch_02/chapter_02.cpp
@@ -3,8 +3,41 @@
 ** Some code snippets from same
 */ 
 
+#include <iostream> // cin, cout, printf
+// consider import statement instead
+
+/*
+** function declarations
+*/
+int procedural_programming();
+bool accept();
+
+/*
+** execution entry point
+*/
 int main ()
 {
-  
+  procedural_programming();
   return 0;
+}
+
+int procedural_programming()
+{
+  std::printf("accept %i\n", accept()); // print the numeric value of boolean returned
+  return 0;
+}
+
+/*
+** 2.3 Procedural Programming
+*/
+
+bool accept()
+{
+  std::cout << "Do you want to proceed (y or n) ?\n";  // write question
+  
+  char answer = 0;                                
+  std::cin >> answer;                                  // read answer
+  
+  if (answer == 'y') return true;                   
+  return false;                                   
 }
