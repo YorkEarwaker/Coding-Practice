b0dd571cb5a9491e5f1801dbd569d959745c6efb
York Earwaker <yorkearwaker@yahoo.com>
2026-07-29 11:33:25 +0100

    bash script success!

--- workflows/shl/fmg/README.md
+++ workflows/shl/fmg/README.md
@@ -5,3 +5,8 @@ stub
 ## Status
 TODO
 * <todo; consider, some file and directories for the change chmod -x to non executable test example. change file_list.txt entries to contain corresponding file paths and names. >
+
+DONE
+* <todo; consider, intent to commit>
+* <todo; consider, run make_file_non_executable.sh in electrical-engineering repo , success! >
+
--- workflows/shl/fmg/make_file_non_exe.sh
+++ workflows/shl/fmg/make_file_non_exe.sh
@@ -5,17 +5,19 @@
 # order of operations
 # read the next line file address
 # if the file exists then
-# display the file executable permission a number and string wrx-
+# display the file executable permission a number 755 and string -rwxrwxr-x
 # change file to non executable show file executable permission num str
 
 # while loops ignore white space and special characters in file names and paths where for loops do not
 # apparently. find definitive source for this claim.
 
-while IFS= -r file; do
+while IFS= read -r file; do
   if [ -f "$file" ]; then
-    stat -c "%a" "%A" "$file" # debug, comment out in prod, set log flag 
-    chmod -x "$file"          # make file non executable for everyone
-    stat -c "%a" "%A" "$file" # debug, comment out in prod, set log flag
+    stat -c "%a" "$file"  # debug, comment out in prod, set log flag 
+    stat -c "%A" "$file"  # debug, comment out in prod, set log flag
+    chmod -x "$file"      # make file non executable for everyone
+    stat -c "%a" "$file"  # debug, comment out in prod, set log flag
+    stat -c "%A" "$file"  # debug, comment out in prod, set log flag
   fi
 done < file_list.txt
 
